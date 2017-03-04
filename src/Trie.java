439ad9389d9c769cc269aa67a3fd8255b9b44baa
MedAlexey <medvedev.lasta@yandex.ru>
2017-03-05 00:04:14 +0800

    Реализовано добавление, удаление и поиск слова

---
+++ src/Trie.java
@@ -0,0 +1,175 @@
+import javax.sound.midi.Track;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Trie {
+
+    static class TrieNode {
+        Object info;         // очередная буква или связанный объект
+        TrieNode son;        // указатель на следующий уровень
+        TrieNode brother;    //указатель на следующий узел того же уровня
+        TrieNode father;     //отец
+        boolean hasBrother;  //есть ли брат
+        boolean hasSon;      //есть ли сын
+        boolean endOfWord;   //является ли концом слова
+        int level;           //на каком уровне расположен
+
+
+         //конструктор узла
+        public TrieNode(Object info,
+                        TrieNode son,
+                        TrieNode brother,
+                        TrieNode father,
+                        boolean hasBrother,
+                        boolean hasSon,
+                        boolean endOfWord,
+                        int level) {
+            this.info = info;
+            this.son = son;
+            this.brother = brother;
+            this.father = father;
+            this.hasBrother = false;
+            this.hasSon = false;
+            this.endOfWord = false;
+            this.level = level;
+        }
+    }
+
+
+    ArrayList<ArrayList<TrieNode>> mainTrie = new ArrayList<ArrayList<TrieNode>>(); //дерево
+
+
+    public void add(String string) {  //добавление
+
+        /** сли в дереве нет даже корня, то создаём корень
+         */
+        if(mainTrie.size() == 0) {
+            ArrayList<TrieNode> firstLvl = new ArrayList<TrieNode>();
+            TrieNode root = new TrieNode(null,null,null,null,false,false,false,0);  //корень
+            firstLvl.add(root);
+            mainTrie.add(firstLvl);
+        }
+
+        /** начальное положение в корне
+         */
+        TrieNode curNode = mainTrie.get(0).get(0);  //узел, в котором мы находимся
+        string = string.toLowerCase();
+
+        for (int i = 0; i < string.length(); i++) {
+            Character nextChar = new Character(string.charAt(i)); //берём следующую букву
+
+            if(curNode.level == 0){
+                if(!curNode.hasSon){
+                    ArrayList<TrieNode> arr = new ArrayList<TrieNode>();
+                    TrieNode son = new TrieNode(nextChar,null,null,curNode, false, false, false, curNode.level + 1);
+                    if (i == string.length()-1) son.endOfWord = true;
+                    arr.add(son);
+                    mainTrie.add(arr);
+                    curNode.son = mainTrie.get(curNode.level+1).get(mainTrie.get(curNode.level+1).size()-1);
+                    curNode.hasSon = true;
+                }
+                curNode = curNode.son;
+            }
+
+
+            while (!curNode.info.equals(nextChar) && curNode.hasBrother) {  //идём по одному уровню
+                curNode = curNode.brother;
+            }
+
+
+            if (!curNode.info.equals(nextChar) && !curNode.hasBrother) { //если дошли до конца уровня и не нашли букву, то добарляем брата
+                TrieNode brother = new TrieNode(nextChar,null,null,curNode.father, false, false, false, curNode.level);
+                if (i == string.length() - 1) brother.endOfWord = true;
+                mainTrie.get(curNode.level).add(brother);
+                curNode.brother = mainTrie.get(curNode.level).get(mainTrie.get(curNode.level).size() - 1); //прикрепляем брата
+                curNode.hasBrother = true;
+                curNode = curNode.brother;
+            }
+            else if (curNode.info.equals(nextChar) && (i == string.length() - 1)) { //если нашли букву и она последняя в слове
+                curNode.endOfWord = true;
+            }
+
+            if(curNode.hasSon) curNode = curNode.son;       //переходим на след уровень
+            else if(i != string.length()-1 && !curNode.hasSon){
+                Character letter = new Character(string.charAt(i+1));   //следующая буква слова
+                TrieNode son = new TrieNode(letter,null,null,curNode, false, false, false, curNode.level + 1);
+
+                if(mainTrie.size() > curNode.level+1) mainTrie.get(curNode.level+1).add(son);
+                else if(mainTrie.size() <= curNode.level + 1){
+                    ArrayList<TrieNode> arr = new ArrayList<TrieNode>();
+                    arr.add(son);
+                    mainTrie.add(arr);
+                }
+
+                curNode.son = mainTrie.get(curNode.level+1).get(mainTrie.get(curNode.level+1).size()-1);
+                curNode.hasSon = true;
+                curNode = curNode.son;
+
+            }
+        }
+
+    }
+
+
+    public boolean find(String string){
+        if(mainTrie.size() == 0) return false;
+
+        TrieNode curNode = mainTrie.get(0).get(0);   //начинаем с вершины
+        string = string.toLowerCase();
+
+        for(int i = 0; i < string.length(); i++){
+            Character nextChar = new Character(string.charAt(i));
+            if (curNode.hasSon) curNode = curNode.son;                 //переходим на нижний уровень
+            else return false;
+
+            while (!curNode.info.equals(nextChar) && curNode.hasBrother){   //пока не найдём нужную букву в уровне
+                curNode = curNode.brother;
+            }
+
+            if (!curNode.info.equals(nextChar)) return false;   //если дошли до конца уровня и не нашли след букву
+            if ((i == string.length()-1) && !curNode.endOfWord) return false;  //если нашли,но поледняя буква не последняя
+        }
+        return true;
+    }
+
+
+
+    public void delete(String string){
+        TrieNode curNode = mainTrie.get(0).get(0);   //начальное положение
+        string = string.toLowerCase();
+
+        if (!find(string)) System.out.print ("Такого слова нет");  //если такого слова нет...
+
+        for (int i = 0; i  <string.length(); i++){    //доходим до последней буквы слова
+            Character nextChar = new Character(string.charAt(i));
+            curNode = curNode.son;
+            while(curNode.hasBrother && !curNode.info.equals(nextChar)){
+                curNode = curNode.brother;
+            }
+        }
+
+        for (int i = string.length(); i > 0; i--){              //идём вверх по уровням, удаляя буквы
+            if (!curNode.hasBrother && !curNode.hasSon) {     //если буква правая крайняя на уровне
+                curNode = curNode.father;
+                mainTrie.get(curNode.level+1).get(mainTrie.get(curNode.level+1).size()-2).brother = null;
+                mainTrie.get(curNode.level+1).get(mainTrie.get(curNode.level+1).size()-2).hasBrother = false; //удалили сведения о брате у брата
+                mainTrie.get(curNode.level+1).remove(mainTrie.get(curNode.level+1).get(mainTrie.get(curNode.level+1).size()-1));
+                if (mainTrie.get(curNode.level+1).size() == 0) mainTrie.remove(curNode.level+1);
+                curNode.son = null;
+                curNode.hasSon = false;
+            }
+            else if (curNode.hasSon && curNode.endOfWord){
+                curNode.endOfWord = false;
+                curNode = curNode.father;
+            }
+            else if (!curNode.hasSon && curNode.hasBrother){
+                curNode.info = '-';
+                curNode.endOfWord = false;
+                curNode = curNode.father;
+            }
+        }
+    }
+    }
